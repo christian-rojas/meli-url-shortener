f2e67315f7f6d8e7c40ef8223abda31519863548
christian-rojas <christian.ici17@gmail.com>
2022-08-26 15:06:12 -0400

    add keep alive connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,19 @@
 import { DynamoDB } from 'aws-sdk';
+import https from 'https'
+
+const agent = new https.Agent({
+  keepAlive: true, 
+  maxSockets: Infinity
+});
 
 // export const client = new DynamoDB({
 //     credentials: 
-//         {
-//             accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-//             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-//         },
-//     region: process.env.AWS_DEFAULT_REGION as string
+//     {
+//         accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+//     },
+//     region: process.env.AWS_DEFAULT_REGION as string,
+//     httpOptions: {agent}
 // })
 
 export const dynamoTable = {
@@ -14,4 +21,4 @@ export const dynamoTable = {
     tableIndexName: process.env.TABLE_INDEX_NAME as string
 }
 
-export const client = new DynamoDB({region: 'us-east-1'})
+export const client = new DynamoDB({region: 'us-east-1', httpOptions: {agent}})
